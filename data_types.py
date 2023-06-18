fe5aa427897037c1bcf885cb2501e4b022234b0a
Peniel <penielfakoya@hotmail.com>
2023-06-18 18:38:03 -0400

    assignment commit

---
+++ data_types.py
@@ -0,0 +1,41 @@
+# Integer
+x = 3
+type_x = type(x)
+print(f"The value of x is {x}, and the data type of x is {type_x}")
+
+# Float
+y = 5.28
+type_y = type(y)
+print(f"The value of y is {y}, and the data type of y is {type_y}")
+
+# Boolean
+z = False
+type_z = type(z)
+print(f"The value of z is {z}, and the data type of z is {type_z}")
+
+# String
+name = "Peniel"
+type_name = type(name)
+print(f"The value of name is {name}, and the data type of name is {type_name}")
+
+# List
+list = [1, 2, 3]
+type_list = type(list)
+print(f"The value of my_list is {list}, and the data type of my_list is {type_list}")
+
+# Tuple
+tuple = (4, 5, 6)
+type_tuple = type(tuple)
+print(f"The value of my_tuple is {tuple}, and the data type of my_tuple is {type_tuple}")
+
+# Dictionary
+dict = {"name": "Peniel", "age": 15}
+type_dict = type(dict)
+print(f"The value of my_dict is {dict}, and the data type of my_dict is {type_dict}")
+
+# Set
+set = {7, 8, 9}
+type_set = type(set)
+print(f"The value of my_set is {set}, and the data type of my_set is {type_set}")
+
+
---
+++ python.py
@@ -0,0 +1,8 @@
+first_name = "Peniel"
+middle_name = "Ojonimi"
+last_name = "Ojomah"
+
+full_name = first_name +" "+ middle_name +" "+ last_name 
+
+print(full_name)
+
